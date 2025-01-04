d4dc09a75da9a2919656f0feb2e850d3e9f650d7
Ndugere <ndungudavid972@gmail.com>
2025-01-04 13:11:29 +0300

    Finished creating the view of each student

--- Student/views.py
+++ Student/views.py
@@ -7,3 +7,8 @@ def index(request):
     return render(request, 'students/index.html', {
         "students" : Stud.objects.all()
     })
+def student_view(request, id):
+    student = Stud.objects.get(pk=id)
+    return render(request, "students/view.html", {
+        "student": student
+    })
