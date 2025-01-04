d33b3aca3be103ca8269f480e300f567a87d2151
Ndugere <ndungudavid972@gmail.com>
2025-01-04 17:38:01 +0300

    I have added the fuctionality of adding a new student

--- Student/views.py
+++ Student/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Stud
+from .forms import StudForm
 
 # Create your views here.
 def index(request):
@@ -11,4 +12,35 @@ def student_view(request, id):
     student = Stud.objects.get(pk=id)
     return render(request, "students/view.html", {
         "student": student
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = StudForm(request.POST)
+        if form.is_valid():
+            new_student_number = form.cleaned_data['student_number']
+            new_first_name = form.cleaned_data['first_name']
+            new_last_name = form.cleaned_data['last_name']
+            new_email = form.cleaned_data['email']
+            new_field_of_study = form.cleaned_data['field_of_study']
+            new_gpa = form.cleaned_data['gpa']
+
+            new_student = Stud(
+                student_number=new_student_number,
+                first_name=new_first_name,
+                last_name=new_last_name,
+                email=new_email,
+                field_of_study=new_field_of_study,
+                gpa=new_gpa,
+            )
+            new_student.save()
+            
+            return render(request, 'students/add.html', {
+                'form': StudForm(),
+                'success': True
+            })
+    else:
+        form = StudForm()
+    return render(request, 'students/add.html', { 
+        'form': form 
     })
